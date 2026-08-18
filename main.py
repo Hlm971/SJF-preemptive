a4034b9391c9bef51cebd7139f1acb658a46bb1b
linhcutis1tg <linhkaka117@gmail.com>
2026-08-18 12:29:08 +0700

    auto test

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 
 from ui import SchedulerApp
+from test_loader import load_test_case
+
 root = tk.Tk()
 
 app = SchedulerApp(root)
 
+app.processes = load_test_case(
+    "test1000.txt"
+)
 root.mainloop()
